8bc25212cc583b3f23b885ff03b8902c8669da7a
Mehmet Emin Hakkoymaz <138326846+MehmetEminHakkoymaz@users.noreply.github.com>
2024-07-30 17:00:54 +0300

    update

---
+++ WpfApp1/EditViewControl.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for EditViewControl.xaml
+    /// </summary>
+    public partial class EditViewControl : UserControl
+    {
+        private MainWindow mainWindow;
+
+        public EditViewControl(MainWindow mainWindow)
+        {
+            InitializeComponent();
+            this.mainWindow = mainWindow;
+        }
+    }
+}
--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -17,14 +17,17 @@ namespace WpfApp1
 {
     public partial class MainWindow : Window
     {
+        private DispatcherTimer checkButtonTimer;
         private DispatcherTimer timer;
         private TimeSpan time;
         private MainControl mainControl; // MainControl'ü sınıf değişkeni olarak tanımlayın
         private ExtendedControl extendedControl; // ExtendedControl'ü sınıf değişkeni olarak tanımlayın
         private ExitGasControl exitGasControl; // ExitGasControl'ü burada bir kez oluşturun
+        private EditViewControl editViewControl; // EditViewControl'ü burada bir kez oluşturun
         public MainWindow()
         {
             InitializeComponent();
+            InitializeTimer();
             StartClock();
             timer = new DispatcherTimer();
             timer.Interval = TimeSpan.FromSeconds(1); // 1 saniyelik aralıklarla güncellenir
@@ -35,6 +38,7 @@ namespace WpfApp1
             mainControl = new MainControl(this); // MainControl'ü burada bir kez oluşturun
             extendedControl = new ExtendedControl(this); // ExtendedControl'ü burada bir kez oluşturun
             exitGasControl = new ExitGasControl(this); // ExitGasControl'ü burada bir kez oluşturun
+            editViewControl = new EditViewControl(this); // EditViewControl'ü burada bir kez oluşturun
         }
         private void Timer_Tick(object sender, EventArgs e)
         {
@@ -43,10 +47,7 @@ namespace WpfApp1
             clockTextBlock.Text = time.ToString(@"hh\:mm\:ss");
         }
 
-        private void StartButton_Click(object sender, RoutedEventArgs e)
-        {
-            timer.Start();
-        }
+
 
         private void Main_Button_Click(object sender, RoutedEventArgs e)
         {
@@ -63,6 +64,11 @@ namespace WpfApp1
             contentArea.Content = exitGasControl;
         }
 
+        private void EditView_Button_Click(object sender, RoutedEventArgs e)
+        {
+            contentArea.Content = editViewControl;
+        }
+
         private void Favourites_Button_Click(object sender, RoutedEventArgs e)
         {
             FavouritesControl favouritesControl = new FavouritesControl();
@@ -75,6 +81,7 @@ namespace WpfApp1
             PumpsControl pumpsControl = new PumpsControl();
             contentArea.Content = pumpsControl;
         }
+
 
         //ÜstMenü
         private void TogglePopupButton_Click(object sender, RoutedEventArgs e)
@@ -148,8 +155,10 @@ namespace WpfApp1
 
         private void FirstStartButton_Click(object sender, RoutedEventArgs e)
         {
-            StartButton.Visibility = Visibility.Visible;
             FirstStartButton.Visibility = Visibility.Collapsed;
+            StartButton.Visibility = Visibility.Visible;
+            StartButton.Opacity = 0.5;
+            StartButton.IsEnabled = false;
 
             mainControl.CheckEllipsePositionAndSetButtonVisibility(mainControl.ellipse1, mainControl.conditionalButton);
             mainControl.CheckEllipsePositionAndSetButtonVisibility(mainControl.ellipse2, mainControl.conditionalButtonStirrer);
@@ -166,6 +175,75 @@ namespace WpfApp1
             extendedControl.CheckEllipsePositionAndSetButtonVisibility(extendedControl.ellipse13, extendedControl.conditionalButtonGas2Flow);
             exitGasControl.CheckEllipsePositionAndSetButtonVisibility(exitGasControl.ellipse14, exitGasControl.conditionalButtonTurbidity);
             exitGasControl.CheckEllipsePositionAndSetButtonVisibility(exitGasControl.ellipse15, exitGasControl.conditionalButtonBalance);
+        }
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            timer.Start();
+            StartButton.Visibility = Visibility.Collapsed;
+            StopButton.Visibility = Visibility.Visible;
+        }
+
+        private void StartButtonContainer_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (!StartButton.IsEnabled)
+            {
+                MessageBox.Show("Paramaters are not same as target value.", "Warning", MessageBoxButton.OK, MessageBoxImage.Warning);
+                e.Handled = true; // Olayın daha fazla işlenmesini engelle
+            }
+        }
+
+        private bool AreVisibleButtonsGreen(params Button[] buttons)
+        {
+            foreach (var button in buttons)
+            {
+                if (button.Visibility == Visibility.Visible)
+                {
+                    if (!(button.Background is SolidColorBrush brush) || brush.Color != Colors.Green)
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        private void InitializeTimer()
+        {
+            checkButtonTimer = new DispatcherTimer();
+            checkButtonTimer.Interval = TimeSpan.FromSeconds(1); // Kontrol aralığını belirleyin
+            checkButtonTimer.Tick += CheckButtonTimer_Tick;
+            checkButtonTimer.Start();
+        }
+
+        private void CheckButtonTimer_Tick(object sender, EventArgs e)
+        {
+            // Sadece görünür olan butonların yeşil olup olmadığını kontrol et
+            if (AreVisibleButtonsGreen(
+                mainControl.conditionalButton,
+                mainControl.conditionalButtonStirrer,
+                mainControl.conditionalButtonpH,
+                mainControl.conditionalButtonpO2,
+                mainControl.conditionalButtonGas1,
+                mainControl.conditionalButtonGas2,
+                mainControl.conditionalButtonGas3,
+                mainControl.conditionalButtonGas4,
+                mainControl.conditionalButtonFoam,
+                extendedControl.conditionalButtonTurbidity,
+                extendedControl.conditionalButtonBalance,
+                extendedControl.conditionalButtonAirFlow,
+                extendedControl.conditionalButtonGas2Flow,
+                exitGasControl.conditionalButtonTurbidity,
+                exitGasControl.conditionalButtonBalance))
+            {
+                StartButton.Opacity = 1.0;
+                StartButton.IsEnabled = true;
+            }
+        }
+
+        private void StopButton_Click(object sender, RoutedEventArgs e)
+        {
+            StopButton.Visibility = Visibility.Collapsed;
+            FirstStartButton.Visibility = Visibility.Visible;
         }
     }
 }
